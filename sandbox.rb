c134b095126f15dbb68dd2134a9ea1f3def237b8
Michael Newman <coloradohokie@gmail.com>
2019-12-21 00:56:26 +0000

    Automatically backed up by Learn

--- sandbox.rb
+++ sandbox.rb
@@ -23,7 +23,9 @@ def nyc_pigeon_organizer(data)
   data.each do |top_key,top_value| 
 #    p top_key, top_value
     top_value.each do |key2, value2|
-      value2.each {|name| p name}
+      value2.each do |name| 
+        
+      end
 #      p key2, value2
     end #nested block
   end # block
