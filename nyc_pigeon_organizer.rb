86b7a9069f31039948b1d93504c780ecf46cdd25
Michael Newman <coloradohokie@gmail.com>
2019-12-21 05:33:24 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -23,16 +23,16 @@ def nyc_pigeon_organizer(data)
 
   pigeon_names.each do |name,value|
     value.each do |k,v|
-      p "name= #{name}, value = #{value} k = #{k}, v = #{v}"
+#      p "name= #{name}, value = #{value} k = #{k}, v = #{v}"
       data[k].each do |subatt, name_list|
-        p "subatt = #{subatt}, name_list= #{name_list}"
+#        p "subatt = #{subatt}, name_list= #{name_list}"
         if name_list.any?{|n| n==name}
           v << subatt.to_s
         end #if
       end #data[k] block
     end #value.each block
   end #pigeon_names.each block
-  pp pigeon_names      
+#  pp pigeon_names      
  # end
   
   return pigeon_names
