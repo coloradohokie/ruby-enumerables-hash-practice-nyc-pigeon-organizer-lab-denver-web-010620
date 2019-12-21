e453f624fef528d563da5ecfcbc472464b88e22d
Michael Newman <coloradohokie@gmail.com>
2019-12-21 00:45:39 +0000

    Automatically backed up by Learn

--- sandbox.rb
+++ sandbox.rb
@@ -20,7 +20,11 @@ pigeon_data = {
 
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
-  data.each { |k,v| p k, v }
+  data.each do |top_key,top_value| 
+    top_key.each do |key2, value2|
+      p key2, value2
+    end #nested block
+  end # block
   
   return pigeon_list
 end
